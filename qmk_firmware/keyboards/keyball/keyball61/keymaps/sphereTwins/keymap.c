ba7f89762fb824660425b184af53b63eef39423c
kazme-egawa <arnerican.f0otboy@gmail.com>
2022-07-17 20:00:10 +0900

    update layout

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/sphereTwins/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/sphereTwins/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_EQL   ,
-    KC_LALT  , LGUI(KC_L)                                  MO(_UTIL), KC_SPC   ,              KC_ENT , KC_DEL   ,                                  KC_GRV   , SCRL_MO
+    KC_LALT  , LGUI(KC_L),                                 MO(_UTIL), KC_SPC   ,              KC_ENT , KC_DEL   ,                                  KC_GRV   , SCRL_MO
   ),
 
   [_UTIL] = LAYOUT_sphere_twins(

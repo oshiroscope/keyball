abae495619f505642c3bdb90fad945c731772c87
oshiroscope <whitecastle4646@gmail.com>
2022-08-04 13:13:51 +0900

    remove unused

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -236,9 +236,6 @@ report_mouse_t pointing_device_driver_get_report(report_mouse_t rep) {
     // report mouse event, if keyboard is primary.
     if (is_keyboard_master() && should_report()) {
         // modify mouse report by PMW3360 motion.
-        if(is_keyboard_left()){
-            
-        }
         motion_to_mouse(&keyball.this_motion, &rep, is_keyboard_left(), keyball.scroll_mode ^ is_keyboard_left());
         motion_to_mouse(&keyball.that_motion, &rep, !is_keyboard_left(), keyball.scroll_mode ^ keyball.this_have_ball ^ is_keyboard_left());
         // store mouse report for OLED.
